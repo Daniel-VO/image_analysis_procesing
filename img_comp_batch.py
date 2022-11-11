9285adfd8ee641026be4f67748d81767c8169bf3
Daniel-VO <71396117+Daniel-VO@users.noreply.github.com>
2022-11-11 20:30:22 +0100

    Add files via upload

--- img_comp_batch.py
+++ img_comp_batch.py
@@ -1,5 +1,5 @@
 """
-Created 10. November 2022 by Daniel Van Opdenbosch, Technical University of Munich
+Created 11. November 2022 by Daniel Van Opdenbosch, Technical University of Munich
 
 This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. It is distributed without any warranty or implied warranty of merchantability or fitness for a particular purpose. See the GNU general public license for more details: <http://www.gnu.org/licenses/>
 """
@@ -21,7 +21,7 @@ files.append(glob.glob('**/*.tif',recursive=True))
 files.append(glob.glob('**/*.TIF',recursive=True))
 files = [y for x in files for y in x]
 
-targetwidth=2048
+target=2048
 
 @ray.remote
 def func(i):
@@ -32,11 +32,15 @@ def func(i):
 		os.system('convert "'+i+'" "'+os.path.splitext(i)[0]+'.png"')
 		os.system('rm "'+i+'"')
 		i=i.replace('.bmp','.png')
+	if '.jpg' in i or '.JPG' in i:
+		operator='-normalize'
+	else:
+		operator='-auto-level'
 	size=str(subprocess.check_output('identify -format "%wx%h" "'+i+'"',shell=True)).split('x')
 	width,height=int(numbers.findall(size[0])[0]),int(numbers.findall(size[1])[0])
-	if width>targetwidth and width>=height:
-		os.system('convert "'+i+'" -colorspace RGB -resize '+str(targetwidth)+'x'+str(int(height*width/targetwidth))+'\> -auto-level "'+i+'"')
-	if height>targetwidth and height>=width:
-		os.system('convert "'+i+'" -colorspace RGB -resize '+str(int(height*width/targetwidth))+'x'+str(targetwidth)+'\> -auto-level "'+i+'"')
+	if width>target and width>=height:
+		os.system('convert "'+i+'" -resize '+str(target)+'x'+str(int(height*width/target))+'\> '+operator+' "'+i+'"')
+	if height>target and height>=width:
+		os.system('convert "'+i+'" -resize '+str(int(height*width/target))+'x'+str(target)+'\> '+operator+' "'+i+'"')
 
 ray.get([func.remote(i) for i in files])
